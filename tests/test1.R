6829dc25a144b37dc3d50b08b49f76fc0c675a7f
Hannes Muehleisen <hannes@muehleisen.org>
2017-07-04 17:00:42 +0200

    actually works

--- tests/test1.R
+++ tests/test1.R
@@ -1,8 +1,14 @@
 #options(chunkrep.len=1000L)#, 
 options(chunkrep.debug=T)
-a <- 1:10^8
+a <- 1:10^7
+a1 <- 10^7:1
+
+
 b <- chunkrep::wrap(a)
+b1 <- chunkrep::wrap(a1)
+
 .Internal(inspect(b))
+.Internal(inspect(b1))
 
 length(b)
 head(b)
@@ -11,4 +17,8 @@ tail(b)
 str(complete.cases(b))
 str(complete.cases(b))
 
+str(complete.cases(b1))
+str(complete.cases(b1))
+
 identical(a, b)
+identical(a1, b1)
